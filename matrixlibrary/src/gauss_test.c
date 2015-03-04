9055b83b09511d6d1ca54e6e990bb5ac7cf03a9c
Johan Isaksson <johis024@student.liu.se>
2015-03-05 00:57:36 +0100

    Gausselemination does almost work

--- matrixlibrary/src/gauss_test.c
+++ matrixlibrary/src/gauss_test.c
@@ -21,6 +21,7 @@ matrix * gauss_eliminate(matrix* Ain,matrix* Bin) {
 
 	//matrix* A = matrix_copy(Ain);
 	matrix* A = Ain;
+	matrix* B = Bin;
 
 
 	/* reach row echelon form */
@@ -35,6 +36,7 @@ matrix * gauss_eliminate(matrix* Ain,matrix* Bin) {
 			for(int ru = r+1; ru <= A->rows; ru++) {
 				if (get_value(ru,r,A) != 0) {
 					switch_rows(r,ru,A);
+					switch_rows(r,ru,B);
 					zero_below = false;
 					break;
 				}
@@ -54,45 +56,78 @@ matrix * gauss_eliminate(matrix* Ain,matrix* Bin) {
 			{
 				continue;
 			}			
-			m = get_value(ru,r,A)/get_value(r,r,A);
 
+			m = get_value(ru,r,A)/get_value(r,r,A);	
+
+			//TODO 	merge theese 2 loops somehow, for better performance
+			//		 	insert value can probably be done in a better way
+			/* insert values in each column */
 			for (int c = r; c <= A->columns; c++) {
 				val = get_value(ru,c,A) - get_value(r,c,A)*m;
 				insert_value(val,ru,c,A);
 			}
+			for (int c = 1; c <= B->columns; c++) {
+				val = get_value(ru,c,B) - get_value(r,c,B)*m;
+				insert_value(val,ru,c,B);
+			}
 			insert_value(0,ru,r,A);
+			divide_row_with_scalar(get_value(r,r,A),r,A);
+			divide_row_with_scalar(get_value(r,r,A),r,B);
 		}
 	}
 
-	/* subtitute backwards */
-	for (int r = A->rows-1; r >= 1; r--) {
-		for (int c = r+1; c <= A->columns; c++) {
-			//TODO alot
-			insert_value(0,r,c,A);
+	divide_row_with_scalar(get_value(A->rows,A->rows,A),A->rows,B);
+	insert_value(1,A->rows,A->rows,A);
+	//divide_row_with_scalar(get_value(A->rows,A->rows,A),A->rows,A);
+
+	/* subtract backwards */
+	for (int r = A->rows; r > 1; r--) {
+		for (int ro = 1; ro < r; ro++) {		
+
+			/* insert zeroes in A and subtrac row in B to the rows above */	
+			m = 1/get_value(ro,r,A);
+			insert_value(0,ro,r,A);
+			for (int c = 1; c <= B->columns; c++) {
+				val = get_value(ro,c,B) - get_value(r,c,B)*m;
+				insert_value(val,ro,c,B);
+			}
+			//TODO add loop if A is not symmetric
 
 		}
 	}
 
+
+	if (B->columns == 1) {
+		return B;
+	}
+	//return A;
 	return A;
 }
 
 int main() {
 
 
-	matrix* A1 = create_matrix(4,4);
-	value temp_a[16] = {1, 2, 3, 4,
-						5, 9, 5, 2,
-						10, 50, 60, 660,
-						10,2,1,1};
-	insert_array(temp_a, A1);
+	matrix* A = create_matrix(3,3);
+	matrix* B = create_matrix(3,1);
 
-	print_matrix(A1);
+	value temp_a[9] = {	1, 2, 3,
+											5, 9, 5,
+											10, 50, 60};
+	insert_array(temp_a, A);
+
+	value temp_b[3] = {	6, 
+											19, 
+											120};
+	insert_array(temp_b, B);
+
+	print_matrix(A);
 
 	matrix* A2;
 
-	A2 = gauss_eliminate(A1,A1);
+	A2 = gauss_eliminate(A,B);
 
 	print_matrix(A2);
+	print_matrix(A);
 
 	return 0;
 
