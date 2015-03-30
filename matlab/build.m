943b4f0acee9947a16e0474597232def35120955
Dennis Ljung <denlj069@ixtab.edu.isy.liu.se>
2015-03-30 16:03:50 +0200

    Mex input/output works like a charm

--- matlab/build.m
+++ matlab/build.m
@@ -4,6 +4,6 @@ mex -I../matrixlibrary/include -c ../quadopt/src/solver.c
 mex -I../matrixlibrary/include -c ../quadopt/src/work_set.c
 mex -I../matrixlibrary/include -c ../quadopt/src/find_lagrange.c
 
-mex -I../matrixlibrary/include -I../quadopt/src -c quadopt.c
+mex -I../matrixlibrary/include -c quadopt.c
 
-mex -L../matrixlibrary/obj -lmatrix solver.o work_set.o find_lagrange.o quadopt.o 
+mex -L../matrixlibrary/obj -lmatrix quadopt.o work_set.o find_lagrange.o solver.o 
