c3a6cdf387201b0cc2a47b477272e84692652c31
mumsaren <denlj069@student.liu.se>
2015-03-30 11:15:22 +0200

    added convert function

--- matlab/quadopt.c
+++ matlab/quadopt.c
@@ -5,7 +5,32 @@ void mexFunction( int nlhs, mxArray *plhs[],
 		  int nrhs, const mxArray *prhs[])
 {  
 
-  /*convert matlab matrises to martin matrises*/
+  //declare variables  
+  mxArray *mat_matrix;
+  matrix *martins_matrix; 
+  matrix[nrhs-1] martins_matrices; 
+  
+
+  //convert matlab matrises to martin matrises
+  for(int i = 0; i < nrhs - 1; i++){
+
+    mat_matrix = prhs[i];
+    int rows = (int)mxGetM(mat_matrix);
+    int columns = (int)mxGetN(mat_matrix);
+    martins_matrix = create_matrix(rows,columns);
+    double *element_ptr = mxGetPr(mat_matrix);
+
+    for(int y = 0; y < columns; y++){
+      for(int x = 0; x < rows; x++){
+
+	insert_value(&element_ptr,x+1, y+1,martins_matrix);
+	element_ptr++; 
+
+    }
+
+    martins_matrices[i] = martins_matrix;   
+   
+  }
 
   /*call solver function from solver.h with martins matrises */
 
