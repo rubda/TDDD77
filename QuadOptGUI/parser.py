e445731cacb1f4c301f7efdf11f66a0b882aa75c
Adam Sestorp <adam.sestorp@gmail.com>
2015-04-01 11:49:08 +0200

    Created subfunctions

--- QuadOptGUI/parser.py
+++ QuadOptGUI/parser.py
@@ -1,10 +1,54 @@
+# TODO:
+# Fixa så att man kan indexera variabler och matriser ?
+# Hur ska data filen se ut då ?
+# kommer ge fel på view_problem funktionerna 
+
 import re
 
 
 start_text = "#include <matLib.h>\n\n"
 
 
-def format_declarations(line):
+def parse_qp(filename, out_filename, data_filename):
+
+    with open(filename) as in_file, open(data_filename) as data_file, open(out_filename, 'w') as out_file:
+
+        out_file.write(start_text)
+        out_file.write("int\nmain()\n{\n\n")
+
+        indent = " "*2
+
+        problem = ""
+        found_min = False
+        copy = False
+
+        for line in in_file:
+            line = line.strip()
+            if line == "parameters" or line == "variables":
+                out_file.write(indent + "/* " + line + " */\n\n")
+                copy = True
+            elif line == "minimize":
+                found_min = True
+            elif line == "subject to":
+                pass
+            elif line == "end":
+                out = "\n"
+                out_file.write(out)
+                copy = False
+            elif found_min:
+                problem = line
+                found_min = False
+            elif copy:
+                out = create_matrices(line)
+                out_file.write(out)
+
+        matrix_variables = fill_matrices(data_file, out_file)
+        create_solver_call(out_file, problem, matrix_variables)
+
+        out_file.write("}")
+
+
+def create_matrices(line):
     indent = " "*2
     line = line.replace(' ', '')
     name = line.split('(')[0]
@@ -20,79 +64,48 @@ def format_declarations(line):
     return string
 
 
-def parse_qp(filename, out_filename, data_filename):
-    problem_file = filename
-    data_file = data_filename
-    output_file = out_filename
+def fill_matrices(data_file, out_file):
+    # Parse the dataFile and insert into matrices
+    # using insert_array(array, matrix) from matLib.h
+
     indent = " "*2
+    data = "{"
+    data_name = ""
+    matrix_name = ""
+    out_file.write(indent + "/* Insert values into matrices */\n\n")
+    finding_data = False
 
-    with open(problem_file) as in_file, open(data_file) as data_file, open(output_file, 'w') as out_file:
+    matrix_variables = []
 
-        copy = False
-        out = start_text
-        out_file.write(out)
-        out = "int\nmain()\n{\n"
-        out_file.write(out)
+    for line in data_file:
+        line = line.strip()
 
-        found_min = False
-        problem = ""
-
-        for line in in_file:
-            print(line)
-            line = line.strip()
-            print(line)
-            if line == "parameters" or line == "variables":
-                out = indent + "/* " + line + " */\n"
-                out_file.write(out)
-                copy = True
-            elif line == "minimize":
-                found_min = True
-            elif line == "subject to":
-                pass
-            elif line == "end":
-                out = "\n"
-                out_file.write(out)
-                copy = False
-            elif found_min:
-                problem = line
-                found_min = False
-            elif copy:
-                out = format_declarations(line)
-                out_file.write(out)
-
-        # Parse the dataFile and insert into matrices
-        # using insert_array(array, matrix)
-        data = "{"
-        data_name = ""
-        matrix_name = ""
-        out_file.write(indent + "/* Insert values into matrices */\n")
-        finding_data = False
-
-        matrix_variables = []
-
-        for line in data_file:
-            line = line.strip()
-
-            if line[:1].isalpha():
-                if finding_data:
-                    data = re.sub(r'\s+', ' ', data)
-                    data = re.sub(r'\s+', ',', data)
-                    out_file.write(indent + "value " + data_name + "[" + str(data[:-1].count(',') + 1) + "]" + " = " + data[:-1] + "};\n")
-                    out_file.write(indent + "insert_array(" + data_name + ", " + matrix_name[:-1] + ");\n")
-                    data = "{"
-                    finding_data = not finding_data
-
-                matrix_name = line[:-1]
-                data_name = matrix_name[:-1] + "_data"
+        if line[:1].isalpha():
+            if finding_data:
+                data = re.sub(r'\s+', ' ', data)
+                data = re.sub(r'\s+', ',', data)
+                out_file.write(indent + "value " + data_name + "[" + str(data[:-1].count(',') + 1) + "]" + " = " + data[:-1] + "};\n")
+                out_file.write(indent + "insert_array(" + data_name + ", " + matrix_name[:-1] + ");\n")
+                data = "{"
                 finding_data = not finding_data
-                matrix_variables.append(matrix_name)
-            else:
-                data = data + line + " "
 
-        # Generate the call or calls to the solver
+            matrix_name = line[:-1]
+            data_name = matrix_name[:-1] + "_data"
+            finding_data = not finding_data
+            matrix_variables.append(matrix_name)
+        else:
+            data = data + line + " "
+
+    return matrix_variables
+
+
+def create_solver_call(out_file, problem, matrix_variables):
+        # Generate the call/calls to the solver
+
+        indent = " "*2
         out_file.write("\n\n" + indent + "/* Solveranropp */ \n\n")
-
         solver_call = indent + "quadOpt("
+
         for var in matrix_variables:
             solver_call += var + ","
 
@@ -112,8 +125,6 @@ def parse_qp(filename, out_filename, data_filename):
         while sum_number > 0:
             out_file.write(solver_call)
             sum_number -= 1
-
-        out_file.write("}")
 
 
 # Get the problem from the .qopt file and convert it to Latex format
