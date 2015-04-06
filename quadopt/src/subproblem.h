d9f371e798e561e842eb759f0c98c31b3eee2fc4
Alexander Yngve <alexander.r.yngve@gmail.com>
2015-04-06 17:33:32 +0200

    Deleted old stuff.

--- quadopt/src/subproblem.h
+++
@@ -1,9 +0,0 @@
-#ifndef SUBPROBLEM_H
-#define SUBPROBLEM_H
-
-#include <matLib.h>
-#include <work_set.h>
-
-void solve_supbroblem(matrix* Ain, matrix* G, matrix* gk, matrix* d, matrix* z, matrix* p, matrix* lagrange, work_set* ws);
-
-#endif /* SUBPROBLEM_H */
