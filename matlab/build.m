d871fe85c8f31fb68a702e479734cc6d68dc96f7
Dennis Ljung <denlj069@ixtab.edu.isy.liu.se>
2015-04-13 11:08:56 +0200

    fixed matlab build file

--- matlab/build.m
+++ matlab/build.m
@@ -2,8 +2,9 @@ mex -setup C
 
 mex -I../matrixlibrary/include -c ../quadopt/src/solver.c
 mex -I../matrixlibrary/include -c ../quadopt/src/work_set.c
-mex -I../matrixlibrary/include -c ../quadopt/src/find_lagrange.c
 
-mex -I../matrixlibrary/include -c quadopt.c
+mex -I../matrixlibrary/include -c ../matlab/quadopt.c
 
-mex -L../matrixlibrary/obj -lmatrix quadopt.o work_set.o find_lagrange.o solver.o 
+mex -L../matrixlibrary/obj -lmatrix quadopt.o work_set.o solver.o 
+
+
