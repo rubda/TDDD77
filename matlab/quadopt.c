2c6b369dfd6bdd9e317df3006a861bab865e987a
Dennis Ljung <denlj069@ixtab.edu.isy.liu.se>
2015-04-13 14:13:39 +0200

    call from matlab works

--- matlab/quadopt.c
+++ matlab/quadopt.c
@@ -14,7 +14,6 @@ void mexFunction( int nlhs, mxArray* plhs[],
   
   
   /*convert matlab matrises to martin matrises*/
-  /*TODO add nrhs - 1* for accuracy argument*/
   int i;
   for(i = 0; i < nrhs; i++){
 
@@ -35,27 +34,38 @@ void mexFunction( int nlhs, mxArray* plhs[],
       }
     }
 
-    martins_matrices[i] = martins_matrix;
+    martins_matrices[i] = martins_matrix
   }
 
+  /*create problem from solver.h with martins matrices */
+  qp_problem* problem = create_problem(martins_matrices[0], martins_matrices[1], martins_matrices[2], martins_matrices[3], martins_matrices[4], martins_matrices[5], martins_matrices[6]);
 
-  /*call solver function from solver.h with martins matrices */
-  
+  /*solve problem*/
+  quadopt_solver(problem);
 
+  result_matrix = problem->solution; 
 
   /*convert resulting matrix to matlab matrix and set output matrix*/
-  plhs[0] = mxCreateDoubleMatrix(martins_matrices[0]->rows,martins_matrices[0]->columns,mxREAL);
+  plhs[0] = mxCreateDoubleMatrix(result_matrix->rows,result_matrix->columns,mxREAL);
 
   out_matrix = mxGetPr(plhs[0]);
   
   int x;
-  for(x = 0; x < martins_matrix->columns; x++){
+  for(x = 0; x < result_matrix->columns; x++){
     int y;
-    for(y = 0; y < martins_matrix->rows; y++){
-      *out_matrix = get_value(y+1,x+1,martins_matrix);
+    for(y = 0; y < result_matrix->rows; y++){
+      *out_matrix = get_value(y+1,x+1,result_matrix);
       out_matrix++;
     }
   }
-  free_matrix(martins_matrix);
+  
+  for(i = 0; i < nrhs; i++){
+    
+    free_matrix(martins_matrices[i]);
+
+  }
+  
+  
+  free_matrix(result_matrix);
  
 }
