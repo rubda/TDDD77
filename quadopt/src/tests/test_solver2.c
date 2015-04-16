0906baa3d1eb98dca89ed609682f16adc1395b92
RubenDas <rubendas.rd@gmail.com>
2015-04-16 18:06:01 +0200

    Changing to double didn't help. Changing mode to anything else makes all assertions fuck up

--- quadopt/src/tests/test_solver2.c
+++ quadopt/src/tests/test_solver2.c
@@ -96,17 +96,31 @@ int main() {
  	       	     0, 
        		     0};
 
+
+  /* MATLABs first iteration optimum point with active set */
   value z0_active_set[4] = {-0.0019, 
 			    -0.8754, 
 			     1.4490, 
 			     0.0996};
 
+  /* MATLABs first iteration optimum point with interior point */
   value z0_interior_point[4] = { 0.2532, 
 			        -0.4397, 
 				 1.2955, 
 				 0.3019};
 
+
+  /* Current start point */
   insert_array(z0_arr, z0);
+
+  /* Optimum should be this, according to MATLAB */
+  matrix* optimum = create_matrix(4, 1);
+  value optimum_arr[4] = {-0.0113
+		       -0.9370,
+		       1.4143,
+		       0.0631};
+  insert_array(optimum_arr, optimum);
+
 
   problem* problem = create_problem(Q,q,E,h,F,g,z0);
 
