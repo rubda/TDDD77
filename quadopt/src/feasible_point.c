fca5afff522eea2cc8345b186618f8e7a2606d64
JohanIsaksson <johis024@student.liu.se>
2015-04-24 17:46:50 +0200

    Wrote new feasible point calculator. solve_linear does not work.

--- quadopt/src/feasible_point.c
+++ quadopt/src/feasible_point.c
@@ -42,9 +42,120 @@ bool is_feasible_point(matrix* z, problem* prob){
   return true;
 }
 
+void rref(matrix* M, matrix* N) {
+  int lead = 1;
+  int i = 1;
+
+  //TODO  doesn't need all theese
+  //      fix indentation
+  matrix* row1;
+  matrix* row2;
+  matrix* row3;
+
+  for (int r = 1; r <= M->rows; r++) {
+    if (M->columns+1 <= lead) {
+      return;
+    }
+    i = r;
+    while (compare_elements(get_value_without_check(i,lead,M),0) == 0) {
+      i = i + 1;
+      if (M->rows+1 == i) {
+        i = r;
+        lead = lead + 1;
+        if (M->columns+1 == lead) {
+          return;
+        }
+      }
+    }
+    switch_rows(i,r,M);
+    switch_rows(i,r,N);
+    if (compare_elements(get_value_without_check(r,lead,M),0) != 0) {
+      divide_row_with_scalar(get_value_without_check(r,lead,M),r,M);
+      divide_row_with_scalar(get_value_without_check(r,lead,M),r,N);
+    }
+    for (i = 1; i <= M->rows; i++) {
+      if (i != r) {
+        row1 = get_row_vector_with_return(r,M);
+        row2 = get_row_vector_with_return(i,M);
+        value val = -get_value_without_check(i,lead,M);
+        multiply_matrix_with_scalar(val,row1);
+        row3 = add_matrices_with_return(row1,row2);
+        insert_row_vector(i,row3,M);
+
+        free_matrix(row1);        
+        free_matrix(row2);
+        free_matrix(row3);
+
+        row1 = get_row_vector_with_return(r,N);
+        row2 = get_row_vector_with_return(i,N);
+        multiply_matrix_with_scalar(val,row1);
+        row3 = add_matrices_with_return(row1,row2);
+        insert_row_vector(i,row3,N);
+
+        free_matrix(row1);
+        free_matrix(row2);
+        free_matrix(row3);
+
+      }
+    }
+    lead++;
+  }
+}
+
+void rref1(matrix* M) {
+  int lead = 1;
+  int i = 1;
+
+  //TODO  doesn't need all theese
+  //      fix indentation
+  matrix* row1 = create_matrix(1,M->columns);
+  matrix* row2 = create_matrix(1,M->columns);
+  matrix* row3 = create_matrix(1,M->columns);
+
+  for (int r = 1; r <= M->rows; r++) {
+    if (M->columns+1 <= lead) {
+      free_matrix(row1);
+      free_matrix(row2);
+      free_matrix(row3);
+      return;
+    }
+    i = r;
+    while (compare_elements(get_value_without_check(i,lead,M),0) == 0) {
+      i = i + 1;
+      if (M->rows+1 == i) {
+        i = r;
+        lead = lead + 1;
+        if (M->columns+1 == lead) {
+          free_matrix(row1);
+          free_matrix(row2);
+          free_matrix(row3);
+          return;
+        }
+      }
+    }
+    switch_rows(i,r,M);
+    if (compare_elements(get_value_without_check(r,lead,M),0) != 0) {
+      divide_row_with_scalar(get_value_without_check(r,lead,M),r,M);
+    }
+    for (i = 1; i <= M->rows; i++) {
+      if (i != r) {
+        get_row_vector(r,M,row1);
+        get_row_vector(i,M,row2);
+        multiply_matrix_with_scalar(-get_value_without_check(i,lead,M),row1);
+        add_matrices(row1,row2,row3);
+        insert_row_vector(i,row3,M);
+      }
+    }
+    lead++;
+  }
+  free_matrix(row1);
+  free_matrix(row2);
+  free_matrix(row3);
+}
+
+
 /* Iterate through all possible combinations of inequality constraints to add */
-void comb(int pool, int need, int* rows, int at, int ri, problem* prob, 
-	  matrix* A, matrix* b, matrix* z, bool* done){
+void comb(int pool, int need, int* rows, int at, int ri, problem* prob, matrix* A, matrix* b, matrix* z, bool* done){
   /* No more slot */
   if (pool < need + at || *done) return;
  
@@ -79,13 +190,39 @@ void comb(int pool, int need, int* rows, int at, int ri, problem* prob,
 
 /* Calculates a feasible starting point for a problem */
 bool find_starting_point(problem* prob){
-  if (prob->equality_count > 0 && prob->inequality_count > 0){    
+  if (prob->equality_count > 0 || prob->inequality_count > 0){    
 
     /* Variables */
     matrix* A = create_matrix(prob->z->rows, prob->z->rows);
     matrix* b = create_matrix(prob->z->rows, 1);
     matrix* tmp_A;
-    value tmp_b;
+    value tmp_b;    
+    work_set* leads = work_set_create(prob->z->rows);
+    work_set* vars_to_zero = work_set_create(prob->z->rows);
+    matrix* Er = matrix_copy(prob->E);
+    matrix* hr = matrix_copy(prob->h);
+
+    /* reduce E so lead values can be retrieved */
+    rref(Er,hr);
+
+    /* get leads */
+    int curr = 1;
+    for (int r = 1; r <= Er->rows; r++) {
+      for (int c = curr; c <= Er->columns; c++) {
+        if (compare_elements(get_value_without_check(r, c, Er),1) == 0) {
+          work_set_append(leads,c);
+          curr = c+1;
+          break;
+        }
+      }
+    }
+
+    /* get variables that can set to 0 later */
+    for (int v = 1; v <= prob->z->rows; v++) {
+      if (!work_set_contains(leads, v)) {
+        work_set_append(vars_to_zero,v);
+      }
+    }
 
     /* Fill A and b with equality constraints */
     int r;
@@ -97,14 +234,75 @@ bool find_starting_point(problem* prob){
       insert_value_without_check(tmp_b, r, 1, b);
     }
 
-    int pool = prob->inequality_count;
+    int pool = prob->inequality_count;    
     int need = A->rows-prob->equality_count;
 
     int* rows = malloc(need*sizeof(int));
     bool done = false;
 
-    comb(pool, need, rows, 0, need, prob, A, b, prob->z0, &done);
+    matrix* new_row = create_matrix(1,A->columns);
+    int count = 0;
 
+    /* find feasible point */
+    while (need > 0) {
+
+      /* iterate through all combinations of inequality constraints */
+      comb(pool, need, rows, 0, need, prob, A, b, prob->z0, &done);
+
+      if (done) {
+        break;
+      } else {
+        free(rows);        
+
+        /* set one variable to 0 */
+        if (count >= vars_to_zero->count) {
+          for (int c = 1; c <= A->columns; c++) {
+            insert_value_without_check(0, 1, c, new_row);          
+          }
+          /* insert this row for good */
+          insert_value_without_check(1, 1, vars_to_zero->data[0], new_row);          
+          insert_row_vector(A->rows-need, new_row, A);
+          insert_value_without_check(0, A->rows-need, 1, b);
+
+          /* insert next row */
+          insert_value_without_check(0, 1, vars_to_zero->data[0], new_row);
+          work_set_remove(vars_to_zero,vars_to_zero->data[0]);
+          count = 1;
+          need--;
+          insert_value_without_check(1, 1, vars_to_zero->data[0], new_row);
+          insert_row_vector(A->rows-need, new_row, A);
+          insert_value_without_check(0, A->rows-need, 1, b);
+
+        } else {
+          if (count == 0) {
+            need--;
+          }
+          for (int c = 1; c <= A->columns; c++) {
+            insert_value_without_check(0, 1, c, new_row);          
+          }
+          insert_value_without_check(1, 1, vars_to_zero->data[count], new_row);
+          insert_row_vector(A->rows-need, new_row, A);
+          insert_value_without_check(0, A->rows-need, 1, b);
+          count++;
+        }   
+
+        rows = malloc(need*sizeof(int));
+
+      }
+    }
+/*
+    print_matrix(A);
+    print_matrix(b);
+
+    printf("%s", done ? "true\n" : "false\n");*/
+
+    if (!done) {
+      //Shall use linear_solve here
+      //done = solve_linear(A, prob->z0, b);
+      least_square(A, prob->z0, b);
+      done = true;
+    }
+    
     free(rows);
     free_matrix(A);
     free_matrix(b);
