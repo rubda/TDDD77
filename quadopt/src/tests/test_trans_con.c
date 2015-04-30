095d5eee83974e41a80a947fe1f1e21ab1b19463
Alexander Yngve <aleyn573@student.liu.se>
2015-05-01 01:01:30 +0200

    E matrix in trans con done.

--- quadopt/src/tests/test_trans_con.c
+++ quadopt/src/tests/test_trans_con.c
@@ -2,26 +2,49 @@
 #include <assert.h>
 #include <matLib.h>
 
+void test_equality_constraints_transform();
+
 int main(){
+  test_equality_constraints_transform();
+
+  return 0;
+}
+
+void test_equality_constraints_transform(){
+  matrix* A = create_matrix(2, 2);
+  value a_arr[4] = {5, 2, 4, 3};
+  insert_array(a_arr, A);
+
+  matrix* B = create_matrix(2, 1);
+  value b_arr[4] = {7, 9};
+  insert_array(b_arr, B);
+
   matrix* k = create_matrix(2, 1);
   value k_arr[2] = {1, 2};
   insert_array(k_arr, k);
 
+  matrix* E = create_zero_matrix(6, 8);
   matrix* h = create_zero_matrix(6, 1);
 
-  matrix* E = create_zero_matrix(6, 8);
-
-  assert(dyn_2_eq(NULL, NULL, k, E, h, 2, 1));
 
   matrix* expected_h = create_matrix(6, 1);
   value eh_arr[6] = {1, 2, 0, 0, 0, 0};
   insert_array(eh_arr, expected_h);
 
+  matrix* expected_E = create_matrix(6, 8);
+  value eE_arr[48] = {1, 0, 0, 0, 0, 0, 0, 0,
+		     0, 1, 0, 0, 0, 0, 0, 0,
+		     -5, -2, 1, 0, 0, 0, 7, 0,
+		     -4, -3, 0, 1, 0, 0, 9, 0,
+		     0, 0, -5, -2, 1, 0, 0, 7,
+		     0, 0, -4, -3, 0, 1, 0, 9};
+  insert_array(eE_arr, expected_E);
+
+  assert(dyn_2_eq(A, B, k, E, h, 2, 1));
+  assert(compare_matrices(E, expected_E));
   assert(compare_matrices(h, expected_h));
 
   free_matrix(k);
   free_matrix(h);
   free_matrix(expected_h);
-
-  return 0;
 }
--- quadopt/src/trans_con.c
+++ quadopt/src/trans_con.c
@@ -11,8 +11,11 @@
 bool check_dim_h(matrix* h);
 bool check_dim_k(matrix* k, size_t card_x);
 bool check_dim_E(matrix* E, size_t card_x, size_t card_u, size_t N);
-bool check_dim_A(matrix* A, size_t card_x, size_t card_u, size_t N);
-bool check_dim_B(matrix* B, size_t card_x, size_t card_u, size_t N);
+bool check_dim_A(matrix* A, size_t card_x);
+bool check_dim_B(matrix* B, size_t card_x, size_t card_u);
+bool insert_identity_matrices(matrix* E, size_t card_x);
+bool insert_A_matrices(matrix* E, matrix* A);
+bool insert_B_matrices(matrix* E, matrix* B, size_t N);
 
 /** Dynamic constraints (A and B with initial values K) transforms to equality constraints (E and h).*/
 bool dyn_2_eq(matrix* A, matrix *B, matrix* k, matrix* E, matrix* h, size_t card_x, size_t card_u){
@@ -20,19 +23,65 @@ bool dyn_2_eq(matrix* A, matrix *B, matrix* k, matrix* E, matrix* h, size_t card
   if(!check_dim_h(h)) return false;
   size_t N = (h->rows - 2) / 2;
   if(!check_dim_E(E, card_x, card_u, N)) return false;
-  if(!check_dim_A(A, card_x, card_u, N)) return false;
-  if(!check_dim_B(B, card_x, card_u, N)) return false;
-
+  if(!check_dim_A(A, card_x)) return false;
+  if(!check_dim_B(B, card_x, card_u)) return false;
 
   if(!insert_sub_matrix(1, k->rows, 1, 1, k, h)) return false;
+  if(!insert_identity_matrices(E, card_x)) return false;
+  multiply_matrix_with_scalar(-1, A);
+  if(!insert_A_matrices(E, A)) return false;
+  if(!insert_B_matrices(E, B, N)) return false;
 
-  /* TODO: Fill E matrix */
+  return true;
+}
+
+bool insert_identity_matrices(matrix* E, size_t card_x){
+  matrix* id_matrix = create_identity_matrix(card_x, card_x);
+  size_t start_col = 1;
+  for(int start_row = 1; start_row < E->rows; start_row += card_x){
+    size_t end_row = start_row + card_x - 1;
+    size_t end_col = start_col + card_x - 1;
+    if(!insert_sub_matrix(start_row, end_row, start_col, end_col, id_matrix, E)){
+      free_matrix(id_matrix);
+      return false;
+    }
+    start_col += card_x;
+  }
+
+  free_matrix(id_matrix);
+  return true;
+}
+
+bool insert_A_matrices(matrix* E, matrix* A){
+  size_t start_col = 1;
+  for(int start_row = 1 + A->rows; start_row < E->rows; start_row += A->rows){
+    size_t end_row = start_row + A->rows - 1;
+    size_t end_col = start_col + A->columns - 1;
+    if(!insert_sub_matrix(start_row, end_row, start_col, end_col, A, E)){
+      return false;
+    }
+    start_col += A->columns;
+  }
+
+  return true;
+}
+
+bool insert_B_matrices(matrix* E, matrix* B, size_t N){
+  size_t start_col = N*B->rows + B->rows + 1;
+  for(int start_row = 1 + B->rows; start_row < E->rows; start_row += B->rows){
+    size_t end_row = start_row + B->rows - 1;
+    size_t end_col = start_col + B->columns - 1;
+    if(!insert_sub_matrix(start_row, end_row, start_col, end_col, B, E)){
+      return false;
+    }
+    start_col += B->columns;
+  }
 
   return true;
 }
 
 bool check_dim_k(matrix* k, size_t card_x){
-  if(k->rows != card_x) return false;
+  if(k->rows != (int)card_x) return false;
   return true;
 }
 
@@ -43,18 +92,20 @@ bool check_dim_h(matrix* h){
 }
 
 bool check_dim_E(matrix* E, size_t card_x, size_t card_u, size_t N){
-  if(E->rows != 2*N+2) return false;
-  if(E->columns != (card_x + card_u)*N + 2) return false;
+  if(E->rows != (int)(2*N+2)) return false;
+  if(E->columns != (int)((card_x + card_u)*N + 2)) return false;
   return true;
 }
 
-bool check_dim_A(matrix* A, size_t card_x, size_t card_u, size_t N){
-  /* TODO: Checks */
+bool check_dim_A(matrix* A, size_t card_x){
+  if(A->rows != (int)card_x) return false;
+  if(A->columns != (int)card_x) return false;
   return true;
 }
 
-bool check_dim_B(matrix* B, size_t card_x, size_t card_u, size_t N){
-  /* TODO: Checks */
+bool check_dim_B(matrix* B, size_t card_x, size_t card_u){
+  if(B->rows != (int)card_x) return false;
+  if(B->columns != (int)card_u) return false;
   return true;
 }
 
