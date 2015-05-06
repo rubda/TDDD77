0d567df4194f2b1b964c8b2be12fe98aa04a6454
Alexander Yngve <aleyn573@student.liu.se>
2015-05-06 16:52:06 +0200

    Disabled slow tests.

--- matrixlibrary/src/tests/test_performance.c
+++ matrixlibrary/src/tests/test_performance.c
@@ -7,20 +7,20 @@ int main(){
   clock_t begin, end;
   double time_spent;
 
-  int number_of_tests=4096;
-  for (int j=1;j<=1;j++){
-    printf("this is test %i regular \n",j);
-  for (int i=4096;i<=number_of_tests;i*=2){
-    to_multiply=create_identity_matrix(i,i);
-    multiplied=create_identity_matrix(i,i);
-    begin = clock();
-    assert(multiply_matrices(to_multiply,to_multiply,multiplied));
-    end = clock();
-    time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-    free_matrix(to_multiply);
-    free_matrix(multiplied);
-    printf(" %f \n",time_spent);
-  }}
+  /* int number_of_tests=4096; */
+  /* for (int j=1;j<=1;j++){ */
+  /*   printf("this is test %i regular \n",j); */
+  /* for (int i=4096;i<=number_of_tests;i*=2){ */
+  /*   to_multiply=create_identity_matrix(i,i); */
+  /*   multiplied=create_identity_matrix(i,i); */
+  /*   begin = clock(); */
+  /*   assert(multiply_matrices(to_multiply,to_multiply,multiplied)); */
+  /*   end = clock(); */
+  /*   time_spent = (double)(end - begin) / CLOCKS_PER_SEC; */
+  /*   free_matrix(to_multiply); */
+  /*   free_matrix(multiplied); */
+  /*   printf(" %f \n",time_spent); */
+  /* }} */
 
 
   return 0;
--- matrixlibrary/src/tests/test_strassen_parallel.c
+++ matrixlibrary/src/tests/test_strassen_parallel.c
@@ -8,16 +8,16 @@ int main(){
   clock_t begin, end;
   double time_spent;
   initialize_parallelization();
-  int shit=4096;
-  int number_of_tests=shit;
-  for (int j=1;j<=1;j++){
-  for (int i=shit;i<=number_of_tests;i*=2){
-    to_multiply=create_identity_matrix(i,i);
-    multiplied=create_identity_matrix(i,i);
-    assert(strassen_matrices_parallel(to_multiply,to_multiply,multiplied));
-    free_matrix(to_multiply);
-    free_matrix(multiplied);
-  }}
+  /* int shit=4096; */
+  /* int number_of_tests=shit; */
+  /* for (int j=1;j<=1;j++){ */
+  /* for (int i=shit;i<=number_of_tests;i*=2){ */
+  /*   to_multiply=create_identity_matrix(i,i); */
+  /*   multiplied=create_identity_matrix(i,i); */
+  /*   assert(strassen_matrices_parallel(to_multiply,to_multiply,multiplied)); */
+  /*   free_matrix(to_multiply); */
+  /*   free_matrix(multiplied); */
+  /* }} */
   deinitialize_parallelization();
 
   return 0;
