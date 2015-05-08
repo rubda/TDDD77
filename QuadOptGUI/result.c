bf3bbad37f5997056988287883b1c7a96955a835
Alexander Yngve <aleyn573@student.liu.se>
2015-05-08 15:05:35 +0200

    Removed checks and made new result.c.

--- QuadOptGUI/result.c
+++ QuadOptGUI/result.c
@@ -1,50 +1,135 @@
 #include <matLib.h>
 #include <solver.h>
+#include <trans_con.h>
+#include <assert.h>
 
 int main(){
-
-  /* Parameters */
-
-  matrix* Q; 
-  Q = create_matrix(4, 4);
-  matrix* q; 
-  q = create_matrix(1, 4);
-  matrix* A; 
-  A = create_matrix(4, 3);
-  matrix* b; 
-  b = create_matrix(1, 3);
-  matrix* F; 
-  F = create_matrix(4, 20);
-  matrix* g; 
-  g = create_matrix(1, 20);
+  size_t card_x = 2;
+  size_t card_u = 1;
+  size_t N = 30;
+  size_t n_vars = card_x*(N + 1) + card_u*N;
 
   /* Variables */
-
   matrix* z; 
-  z = create_matrix(4, 1);
+  z = create_matrix(n_vars, 1);
 
-
-
-  /* Insert values into matrices */
-
-  value Q_data[16] = {10,0,0,0,0,2,0,0,0,0,5,0,0,0,0,100};
+  /* Coefficients */
+  matrix* Q; 
+  Q = create_matrix(2, 2);
+  value Q_data[4] = {4, 0, 4, 0};
   insert_array(Q_data, Q);
-  value q_data[4] = {0.5751,0.7510,0.1535,0.3568};
-  insert_array(q_data, q);
-  value A_data[12] = {-0.9163,-0.1967,-0.0123,0.6752,1.7876,-0.8901,0.0728,0.7860,-0.8204,0.9107,0.9394,-2.1327};
-  insert_array(A_data, A);
-  value b_data[3] = {0.2199,0.9663,0.3500};
-  insert_array(b_data, b);
-  value F_data[80] = {0.9018,0.0317,-1.0579,-0.6312,2.1212,0.3413,-0.6434,-0.5003,1.2486,-0.0835,0.2584,-0.8672,-1.1232,0.6164,0.8917,-1.0401,-0.8301,-0.5250,-0.8366,1.2654,-0.1218,1.0077,0.5531,-0.2415,-0.6426,1.8291,1.4584,-1.7290,-0.0789,0.0853,-0.8551,-0.4882,1.2281,-0.0683,-0.9921,1.0604,-0.5314,-0.5635,-0.0117,-0.5382,-0.2861,-0.4736,0.6269,1.7773,-0.2276,-1.7035,0.0015,-0.7795,0.6745,-0.0537,-0.8163,-0.7530,1.0368,-0.8813,0.1151,-1.0331,-0.1495,1.2559,0.2071,1.1638,-0.3171,0.1558,-0.4446,-0.5801,0.9336,0.0549,-1.1205,0.4173,0.7233,1.3986,0.4354,-1.6481,0.4882,-1.7756,0.0171,-0.5727,3.2662,0.3297,-0.3630,0.6592};
-  insert_array(F_data, F);
 
+  matrix* q;
+  q = create_matrix(2, 1);
+  value q_data[2] = {0, 0};
+  insert_array(q_data, q);
+
+  matrix* P;
+  P = create_matrix(2, 2);
+  value P_data[4] = {59.2896, 6.9782,
+		     6.9782, 1.8280};
+  insert_array(P_data, P);
+
+  matrix* R;
+  R = create_matrix(1, 1);
+  value R_data[1] = {8};
+  insert_array(R_data, R);
+
+
+  /* Parameters */
+  matrix* A;
+  A = create_matrix(2, 2);
+  value A_data[4] = {0.9721, 0.0155,
+		     0.2114, 0.9705};
+  insert_array(A_data, A);
+
+  matrix* B;
+  B = create_matrix(2, 1);
+  value B_data[2] = {0.0143, 0.6860};
+
+  matrix* Fx;
+  Fx = create_matrix(12, 2);
+  value Fx_data[24] = {-0.8037, -0.0461,
+		      -0.8442, -0.0404,
+		      -0.8845, -0.0332,
+		      0.9243, 0.0243,
+		      -0.9243, -0.0243,
+		      0.9630, 0.0133,
+		      -0.9630, -0.0133,
+		      1.0000, 0,
+		      0, 1.0000,
+		      -1.0000, 0,
+		      0, -1.0000,
+		      0.6398, 0.1555};
+  insert_array(Fx_data, Fx);
+
+  matrix* gx;
+  gx = create_matrix(12, 1);
+  value gx_data[12] =    {15,
+			 15,
+			 15,
+			 20,
+			 15,
+			 20,
+			 15,
+			 20,
+			 100,
+			 15,
+			 100,
+			 25};
+  insert_array(gx_data, gx);
+
+  matrix* Fu;
+  Fu = create_matrix(1, 1);
+  value Fu_data[1] = {0};
+  insert_array(Fu_data, Fu);
+
+  matrix* gu;
+  gu = create_matrix(1, 1);
+  value gu_data[1] = {0};
+  insert_array(gu_data, gu);
+
+  matrix* k;
+  k = create_matrix(2, 1);
+  value k_data[2] = {15, 45};
+  insert_array(k_data, k);
+
+
+  /* Transform matrix stuff */
+  matrix* E = create_zero_matrix(card_x*(N + 1), n_vars);
+  matrix* h = create_zero_matrix(card_x*(N + 1), 1);
+  assert(trans_dyn_cons(A, B, k, E, h, card_x, card_u));
+  print_matrix(E);
+  print_matrix(h);
+  
+  matrix* F = create_zero_matrix(Fx->rows + Fu->rows, Fx->columns + Fu->columns);
+  matrix* g = create_zero_matrix(gx->rows + gu->rows, 1);
+  assert(trans_ineq_cons(Fx, Fu, gx, gu, F, g));
+  print_matrix(F);
+  print_matrix(g);
 
   /* Solveranropp */ 
-
   int i; 
   for(i = 0; i <= 10; i++){ 
-    problem* problem = create_problem(Q ,q ,A ,b ,F ,g ,0,1);
+    problem* problem = create_problem(Q, q, E, h, F, g, NULL, 0, 0);
     quadopt_solver(problem);
     print_solution(problem);
- } 
+  }
+
+  free_matrix(Q);
+  free_matrix(q);
+  free_matrix(P);
+  free_matrix(R);
+  free_matrix(A);
+  free_matrix(B);
+  free_matrix(Fx);
+  free_matrix(gx);
+  free_matrix(Fu);
+  free_matrix(gu);
+  free_matrix(k);
+  free_matrix(E);
+  free_matrix(h);
+  free_matrix(F);
+  free_matrix(g);
 }
+  
--- quadopt/src/trans_con.c
+++ quadopt/src/trans_con.c
@@ -19,12 +19,7 @@ bool insert_B_matrices(matrix* E, matrix* B, size_t N);
 
 /** Dynamic constraints (A and B with initial values K) transforms to equality constraints (E and h).*/
 bool trans_dyn_cons(matrix* A, matrix *B, matrix* k, matrix* E, matrix* h, size_t card_x, size_t card_u){
-  /* if(!check_dim_k(k, card_x)) return false; */
-  /* if(!check_dim_h(h)) return false; */
   size_t N = (h->rows - 2) / 2;
-  /* if(!check_dim_E(E, card_x, card_u, N)) return false; */
-  /* if(!check_dim_A(A, card_x)) return false; */
-  /* if(!check_dim_B(B, card_x, card_u)) return false; */
 
   if(!insert_sub_matrix(1, k->rows, 1, 1, k, h)) return false;
   if(!insert_identity_matrices(E, card_x)) return false;
@@ -89,33 +84,3 @@ bool insert_B_matrices(matrix* E, matrix* B, size_t N){
 
   return true;
 }
-
-bool check_dim_k(matrix* k, size_t card_x){
-  if(k->rows != (int)card_x) return false;
-  return true;
-}
-
-bool check_dim_h(matrix* h){
-  if(h->columns != 1) return false;
-  if(h->rows % 2 != 0) return false;
-  return true;
-}
-
-bool check_dim_E(matrix* E, size_t card_x, size_t card_u, size_t N){
-  if(E->rows != (int)(2*N+card_x)) return false;
-  if(E->columns != (int)((card_x + card_u)*N + 2)) return false;
-  return true;
-}
-
-bool check_dim_A(matrix* A, size_t card_x){
-  if(A->rows != (int)card_x) return false;
-  if(A->columns != (int)card_x) return false;
-  return true;
-}
-
-bool check_dim_B(matrix* B, size_t card_x, size_t card_u){
-  if(B->rows != (int)card_x) return false;
-  if(B->columns != (int)card_u) return false;
-  return true;
-}
-
