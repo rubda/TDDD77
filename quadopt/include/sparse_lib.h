63b15cc187021eae7e4139a6750c311012f24621
RubenDas <rubendas.rd@gmail.com>
2015-05-12 15:40:25 +0200

    Sorry Travis, fixes in sparse stuff

--- quadopt/include/sparse_lib.h
+++ quadopt/include/sparse_lib.h
@@ -1,3 +1,6 @@
+#ifndef SPARSE_LIB_H
+#define SPARSE_LIB_H
+
 #include <matLib.h>
 
 /* store sparse matrix using COO */
@@ -17,7 +20,7 @@ sparse_matrix* create_sparse_matrix(matrix* Ain, int size);
 
 sparse_matrix* create_empty_sparse_matrix(int size);
 
-int matrix_sparsity(matrix* A);
+size_t matrix_sparsity(matrix* A);
 
 bool multiply_sparse_matrix_vector(sparse_matrix* A, matrix* x, matrix* Ax);
 
@@ -35,3 +38,5 @@ void free_sparse_matrix(sparse_matrix* S);
 
 /* solves Ax = b, x should be set to 0 */
 bool conjugate_gradient(sparse_matrix* A, matrix* x, matrix* b);
+
+#endif /* SPARSE_LIB_H */
