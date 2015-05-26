ab881b420167d46bd1af223464c22e6e0a9688f4
RubenDas <rubendas.rd@gmail.com>
2015-05-26 14:38:49 +0200

    Gör om detta sen, måste kolla github

--- QuadOptGUI/result.c
+++ QuadOptGUI/result.c
@@ -15,41 +15,41 @@ size_t n_vars = card_x*(N+1) + card_u*N;
 matrix* z = create_matrix(n_vars, 1);
 
 /* Coeffients */
-matrix* gx = create_matrix(12,1);
-value gx_data[12] = {15,15,15,20,15,20,15,20,100,15,100,25};
-insert_array(gx_data, gx);
-
-matrix* Fx = create_matrix(12,2);
-value Fx_data[24] = {-0.8037,-0.0461,-0.8442,-0.0404,-0.8845,-0.0332,0.9243,0.0243,-0.9243,-0.0243,0.9630,0.0133,-0.9630,-0.0133,1.0000,0,0,1.0000,-1.0000,0,0,-1.0000,0.6398,0.1555};
-insert_array(Fx_data, Fx);
-
-matrix* B = create_matrix(2,1);
-value B_data[2] = {0.0143,0.6860};
-insert_array(B_data, B);
-
-matrix* u_lim = create_matrix(2,2);
-value u_lim_data[4] = {25,25};
-insert_array(u_lim_data, u_lim);
+matrix* A = create_matrix(2,2);
+value A_data[4] = {0.9721,0.0155,0.2114,0.9705};
+insert_array(A_data, A);
 
 matrix* Q = create_matrix(2,2);
 value Q_data[4] = {4.0000,0,0,0.1000};
 insert_array(Q_data, Q);
 
+matrix* B = create_matrix(2,1);
+value B_data[2] = {0.0143,0.6860};
+insert_array(B_data, B);
+
 matrix* P = create_matrix(2,2);
 value P_data[4] = {59.2896,6.9782,6.9782,1.8280};
 insert_array(P_data, P);
+
+matrix* Fx = create_matrix(12,2);
+value Fx_data[24] = {-0.8037,-0.0461,-0.8442,-0.0404,-0.8845,-0.0332,0.9243,0.0243,-0.9243,-0.0243,0.9630,0.0133,-0.9630,-0.0133,1.0000,0,0,1.0000,-1.0000,0,0,-1.0000,0.6398,0.1555};
+insert_array(Fx_data, Fx);
 
 matrix* x_lim = create_matrix(4,4);
 value x_lim_data[16] = {20,100,15,100};
 insert_array(x_lim_data, x_lim);
 
+matrix* u_lim = create_matrix(2,2);
+value u_lim_data[4] = {25,25};
+insert_array(u_lim_data, u_lim);
+
+matrix* gx = create_matrix(12,1);
+value gx_data[12] = {15,15,15,20,15,20,15,20,100,15,100,25};
+insert_array(gx_data, gx);
+
 matrix* R = create_matrix(1,1);
 value R_data[1] = {8};
 insert_array(R_data, R);
-
-matrix* A = create_matrix(2,2);
-value A_data[4] = {0.9721,0.0155,0.2114,0.9705};
-insert_array(A_data, A);
 
 matrix* k = create_matrix(2,1);
 value k_data[2] = {15,45};
@@ -79,15 +79,15 @@ quadopt_solver(problem);
 print_solution(problem);
 }
 
-free_matrix(gx);
-free_matrix(Fx);
-free_matrix(B);
-free_matrix(u_lim);
-free_matrix(Q);
-free_matrix(P);
-free_matrix(x_lim);
-free_matrix(R);
 free_matrix(A);
+free_matrix(Q);
+free_matrix(B);
+free_matrix(P);
+free_matrix(Fx);
+free_matrix(x_lim);
+free_matrix(u_lim);
+free_matrix(gx);
+free_matrix(R);
 free_matrix(k);
 free_matrix(F);
 free_matrix(g);
